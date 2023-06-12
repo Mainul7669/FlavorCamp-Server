2fcc0ffc96a833fdf8bca257792967659819543a
Mainul7669 <mainul7669@gmail.com>
2023-06-12 11:25:56 +0600

    mongod added

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyio3kh.mongodb.net/?retryWrites=true&w=majority`;
