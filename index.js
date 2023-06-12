de33fdaed72695cca6c8b42e17abf541261621ca
Mainul7669 <mainul7669@gmail.com>
2023-06-12 11:26:51 +0600

    users gotten

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyio3kh.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // await client.connect();
+  
+      const toysCollection = client.db("toysDB").collection("toys");
+      const MyToysCollection = client.db("toysDB").collection("MyToys");
+  
+      // For Category Section
+      app.get("/toys", async (req, res) => {
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+  
+      app.get("/toys/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await toysCollection.findOne(query);
+        res.send(result);
+      });
